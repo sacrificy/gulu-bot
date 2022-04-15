506b5fc2349a4b3d49dac2efda114f1114ed5cbb
yixuanhe <yixuanhe@trip.com>
2022-04-16 03:13:26 +0800

    test

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.post('/invite', function (req, res) {
   res.json(req.headers)
 })
 
+app.post('/hello', function (req, res) {
+  const { i } = req.body
+  // console.log(req.body)
+  res.json(req.headers)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
