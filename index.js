d46f13df08284b61aa22666698838cbd873cd7d3
yixuanhe <yixuanhe@trip.com>
2022-04-16 00:02:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { invite } from './discord/index.js'
+
+const app = express()
+const port = 3003
+
+app.use(express.json())
+
+app.post('/invite', function (req, res) {
+  const { i } = req.body
+  console.log(req.body)
+  res.json(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
